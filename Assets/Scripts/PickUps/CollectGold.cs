8359a4d645767bbdad250b1fb9733142d61de281
Limesak <morgan.cnudde@gmail.com>
2022-01-26 13:19:35 +0100

    Post process tweak + ajustement collectible gold

---
+++ Assets/Scripts/PickUps/CollectGold.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsteroidBelt
+{
+    public class CollectGold : CollectMe 
+    {
+
+        public float movementSpeed = 4f;
+        public int scoreToGive;
+        public Rigidbody rb;
+
+        void Update(){
+            if(transform.position.y < -13) RemoveFromGame();
+        }
+
+        void Start(){
+            rb.velocity = transform.up * -movementSpeed;
+        }
+
+        public override void CollectThis(){
+            Main.Instance.currentScore += scoreToGive;
+        }
+
+        void RemoveFromGame(){
+            Destroy(gameObject);
+        }
+    }
+}
