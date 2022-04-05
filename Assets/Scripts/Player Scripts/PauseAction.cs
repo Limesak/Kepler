ab23692d7acdbea0af13fff4a80ecd58ad596472
Limesak <morgan.cnudde@gmail.com>
2022-04-05 10:30:06 +0200

    fix inputs

--- Assets/Scripts/Player Scripts/PauseAction.cs
+++ Assets/Scripts/Player Scripts/PauseAction.cs
@@ -6,14 +6,12 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-namespace AsteroidBelt.Player_Scripts
+public class @PauseAction : IInputActionCollection, IDisposable
 {
-    public class @PauseAction : IInputActionCollection, IDisposable
+    public InputActionAsset asset { get; }
+    public @PauseAction()
     {
-        public InputActionAsset asset { get; }
-        public @PauseAction()
-        {
-            asset = InputActionAsset.FromJson(@"{
+        asset = InputActionAsset.FromJson(@"{
     ""name"": ""PauseAction"",
     ""maps"": [
         {
@@ -44,7 +42,7 @@ namespace AsteroidBelt.Player_Scripts
                 {
                     ""name"": """",
                     ""id"": ""ffea5534-f76f-4fde-a7aa-afc38654ec19"",
-                    ""path"": ""<Keyboard>/escape"",
+                    ""path"": ""<Keyboard>/p"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -57,90 +55,89 @@ namespace AsteroidBelt.Player_Scripts
     ],
     ""controlSchemes"": []
 }");
-            // Pause
-            m_Pause = asset.FindActionMap("Pause", throwIfNotFound: true);
-            m_Pause_PauseGame = m_Pause.FindAction("PauseGame", throwIfNotFound: true);
-        }
-
-        public void Dispose()
-        {
-            UnityEngine.Object.Destroy(asset);
-        }
-
-        public InputBinding? bindingMask
-        {
-            get => asset.bindingMask;
-            set => asset.bindingMask = value;
-        }
-
-        public ReadOnlyArray<InputDevice>? devices
-        {
-            get => asset.devices;
-            set => asset.devices = value;
-        }
-
-        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
-
-        public bool Contains(InputAction action)
-        {
-            return asset.Contains(action);
-        }
-
-        public IEnumerator<InputAction> GetEnumerator()
-        {
-            return asset.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public void Enable()
-        {
-            asset.Enable();
-        }
-
-        public void Disable()
-        {
-            asset.Disable();
-        }
-
         // Pause
-        private readonly InputActionMap m_Pause;
-        private IPauseActions m_PauseActionsCallbackInterface;
-        private readonly InputAction m_Pause_PauseGame;
-        public struct PauseActions
+        m_Pause = asset.FindActionMap("Pause", throwIfNotFound: true);
+        m_Pause_PauseGame = m_Pause.FindAction("PauseGame", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Pause
+    private readonly InputActionMap m_Pause;
+    private IPauseActions m_PauseActionsCallbackInterface;
+    private readonly InputAction m_Pause_PauseGame;
+    public struct PauseActions
+    {
+        private @PauseAction m_Wrapper;
+        public PauseActions(@PauseAction wrapper) { m_Wrapper = wrapper; }
+        public InputAction @PauseGame => m_Wrapper.m_Pause_PauseGame;
+        public InputActionMap Get() { return m_Wrapper.m_Pause; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PauseActions set) { return set.Get(); }
+        public void SetCallbacks(IPauseActions instance)
         {
-            private @PauseAction m_Wrapper;
-            public PauseActions(@PauseAction wrapper) { m_Wrapper = wrapper; }
-            public InputAction @PauseGame => m_Wrapper.m_Pause_PauseGame;
-            public InputActionMap Get() { return m_Wrapper.m_Pause; }
-            public void Enable() { Get().Enable(); }
-            public void Disable() { Get().Disable(); }
-            public bool enabled => Get().enabled;
-            public static implicit operator InputActionMap(PauseActions set) { return set.Get(); }
-            public void SetCallbacks(IPauseActions instance)
+            if (m_Wrapper.m_PauseActionsCallbackInterface != null)
             {
-                if (m_Wrapper.m_PauseActionsCallbackInterface != null)
-                {
-                    @PauseGame.started -= m_Wrapper.m_PauseActionsCallbackInterface.OnPauseGame;
-                    @PauseGame.performed -= m_Wrapper.m_PauseActionsCallbackInterface.OnPauseGame;
-                    @PauseGame.canceled -= m_Wrapper.m_PauseActionsCallbackInterface.OnPauseGame;
-                }
-                m_Wrapper.m_PauseActionsCallbackInterface = instance;
-                if (instance != null)
-                {
-                    @PauseGame.started += instance.OnPauseGame;
-                    @PauseGame.performed += instance.OnPauseGame;
-                    @PauseGame.canceled += instance.OnPauseGame;
-                }
+                @PauseGame.started -= m_Wrapper.m_PauseActionsCallbackInterface.OnPauseGame;
+                @PauseGame.performed -= m_Wrapper.m_PauseActionsCallbackInterface.OnPauseGame;
+                @PauseGame.canceled -= m_Wrapper.m_PauseActionsCallbackInterface.OnPauseGame;
+            }
+            m_Wrapper.m_PauseActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @PauseGame.started += instance.OnPauseGame;
+                @PauseGame.performed += instance.OnPauseGame;
+                @PauseGame.canceled += instance.OnPauseGame;
             }
         }
-        public PauseActions @Pause => new PauseActions(this);
-        public interface IPauseActions
-        {
-            void OnPauseGame(InputAction.CallbackContext context);
-        }
+    }
+    public PauseActions @Pause => new PauseActions(this);
+    public interface IPauseActions
+    {
+        void OnPauseGame(InputAction.CallbackContext context);
     }
 }
